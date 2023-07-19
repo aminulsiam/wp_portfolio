02a80d4cf759027db4e1e78ecc8b898bd3928e6f
aminulsiam <aminulhossain90@gmail.com>
2023-07-19 20:26:16 +0600

    Add meta data without thumbnail

--- class-plugin-admin.php
+++ class-plugin-admin.php
@@ -71,24 +71,34 @@ class WP_Portfolio_Admin {
 	 * @param $post
 	 */
 	public function pf_render_metabox( $post ) {
+
+		$pf_ex_url      = get_post_meta( $post->ID, 'pf_ex_url', true );
+		$pf_title       = get_post_meta( $post->ID, 'pf_title', true );
+		$pf_description = get_post_meta( $post->ID, 'pf_description', true );
+
 		?>
         <form action="" method="post">
             <div class="container">
 
-                <label for="email"><b>External Url</b></label>
-                <input type="text" placeholder="Enter External Url" name="" id="" required>
-
+                <label for="pf_ex_url"><b>External Url</b></label>
+                <input type="text" placeholder="Enter External Url" name="pf_ex_url" id="ex_url"
+                       value="<?php esc_attr_e( $pf_ex_url ); ?>" required>
 
                 <label for="psw"><b>Title</b></label>
-                <input type="text" placeholder="Enter Title" name="" id="" required>
+                <input type="text" placeholder="Enter Title" name="pf_title" id="pf_title"
+                       value="<?php esc_attr_e( $pf_title ); ?>" required>
 
                 <label for="psw"><b>Description</b></label>
-                <textarea name="" id="" cols="30" rows="10" placeholder="Write Description"></textarea>
+                <textarea name="pf_description" id="" cols="30" rows="10" placeholder="Write Description">
+                    <?php esc_attr_e( $pf_description ); ?>
+                </textarea>
 
                 <label for="psw"><b>Thumbnail Image</b></label>
                 <input type="file" accept="image/*" id="preview-images" name="preview-images" multiple>
 
                 <span id="preview-container"></span>
+
+				<?php wp_nonce_field( 'pf_meta_box_nonce', 'pf_meta_box_nonce' ); ?>
 
             </div>
 
@@ -98,25 +108,53 @@ class WP_Portfolio_Admin {
 
 
 	/**
-     * Save metadata with projects cpt
-     *
 	 * @param $post_id
+	 *
+	 * @return mixed
 	 */
 	public function pf_save_metadata( $post_id ) {
+
+		if ( ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) ) {
+			return $post_id;
+		}
+
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return $post_id;
+		}
+
+		if ( ! isset( $_POST['pf_meta_box_nonce'] ) ) {
+			return $post_id;
+		}
+
+		// Verify that the nonce is valid.
+		if ( ! wp_verify_nonce( 'pf_meta_box_nonce', 'pf_meta_box_nonce' ) ) {
+			return $post_id;
+		}
+
+		$pf_ex_url      = isset( $_POST['pf_ex_url'] ) ? sanitize_text_field( $_POST['pf_ex_url'] ) : "";
+		$pf_title       = isset( $_POST['pf_title'] ) ? sanitize_text_field( $_POST['pf_title'] ) : "";
+		$pf_description = isset( $_POST['pf_description'] ) ? sanitize_text_field( $_POST['pf_description'] ) : "";
+
+		update_post_meta( $post_id, 'pf_ex_url', $pf_ex_url );
+		update_post_meta( $post_id, 'pf_title', $pf_title );
+		update_post_meta( $post_id, 'pf_description', $pf_description );
+
 
 	} //end pf_save_metadata
 
 	/**
 	 * Register all styles
 	 */
-	public function pf_enqueue_styles() {
+	public
+	function pf_enqueue_styles() {
 		wp_enqueue_style( 'pf-admin-css', plugins_url( '/assets/css/wppool-admin.css', __FILE__ ), array(), time(), 'all' );
 	}
 
 	/**
 	 * Register all scripts
 	 */
-	public function pf_enqueue_scripts() {
+	public
+	function pf_enqueue_scripts() {
 
 		$page = isset( $_REQUEST['page'] ) ? sanitize_text_field( $_REQUEST['page'] ) : '';
 
