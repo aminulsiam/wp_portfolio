449a254bbc0c8a23193136665a317e3f2a02d616
aminulsiam <aminulhossain90@gmail.com>
2023-07-19 19:39:09 +0600

    Add Meta Data

---
+++ class-plugin-admin.php
@@ -0,0 +1,133 @@
+<?php
+
+class WP_Portfolio_Admin {
+
+	/**
+	 * Wp_Pool_Admin constructor.
+	 * Write all admin hooks
+	 */
+	public function __construct() {
+		add_action( 'admin_enqueue_scripts', array( $this, 'pf_enqueue_styles' ), 10 );
+		add_action( 'admin_enqueue_scripts', [ $this, 'pf_enqueue_scripts' ] );
+
+
+		add_action( 'init', [ $this, 'pf_register_projects_cpt' ] );
+		add_action( 'add_meta_boxes', [ $this, 'pf_add_meta_box' ] );
+		add_action( 'save_post', [ $this, 'pf_save_metadata' ] );
+
+
+	}
+
+	/**
+	 * Register Projects Custom Post Types
+	 *
+	 * @no_param
+	 */
+	public function pf_register_projects_cpt() {
+
+		$labels = array(
+			'name'               => _x( 'Projects', 'pf' ),
+			'add_new_item'       => _x( 'Add New Project', 'pf' ),
+			'all_items'          => __( 'All Projects', 'pf' ),
+			'view_item'          => __( 'View Project', 'pf' ),
+			'add_new'            => __( 'Add New', 'pf' ),
+			'edit_item'          => __( 'Edit Project', 'pf' ),
+			'update_item'        => __( 'Update Project', 'pf' ),
+			'search_items'       => __( 'Search Project', 'pf' ),
+			'not_found'          => __( 'Not Found', 'pf' ),
+			'not_found_in_trash' => __( 'Not found in Trash', 'pf' ),
+
+		);
+
+		$args = array(
+			'public'    => true,
+			'labels'    => $labels,
+			'menu_icon' => 'dashicons-layout',
+			'supports'  => array( 'title', 'editor', 'thumbnail' ),
+			'rewrite'   => array( 'slug' => 'service_management' ),
+
+		);
+		register_post_type( 'projects', $args );
+
+	} //end method pf_register_projects_cpt
+
+	/**
+	 *
+	 */
+	public function pf_add_meta_box() {
+		add_meta_box(
+			'pf_projects',
+			'Projects Additional Information',
+			[ $this, 'pf_render_metabox' ],
+			'projects',
+			'normal',
+			'default'
+		);
+	} //end method pf_add_meta_in_projects_cpt
+
+	/**
+	 * Projects Meta Box Renderer
+	 *
+	 * @param $post
+	 */
+	public function pf_render_metabox( $post ) {
+		?>
+        <form action="" method="post">
+            <div class="container">
+
+                <label for="email"><b>External Url</b></label>
+                <input type="text" placeholder="Enter External Url" name="" id="" required>
+
+
+                <label for="psw"><b>Title</b></label>
+                <input type="text" placeholder="Enter Title" name="" id="" required>
+
+                <label for="psw"><b>Description</b></label>
+                <textarea name="" id="" cols="30" rows="10" placeholder="Write Description"></textarea>
+
+                <label for="psw"><b>Thumbnail Image</b></label>
+                <input type="file" accept="image/*" id="preview-images" name="preview-images" multiple>
+
+                <span id="preview-container"></span>
+
+            </div>
+
+        </form>
+		<?php
+	} //end function pf_render_metabox
+
+
+	/**
+     * Save metadata with projects cpt
+     *
+	 * @param $post_id
+	 */
+	public function pf_save_metadata( $post_id ) {
+
+	} //end pf_save_metadata
+
+	/**
+	 * Register all styles
+	 */
+	public function pf_enqueue_styles() {
+		wp_enqueue_style( 'pf-admin-css', plugins_url( '/assets/css/wppool-admin.css', __FILE__ ), array(), time(), 'all' );
+	}
+
+	/**
+	 * Register all scripts
+	 */
+	public function pf_enqueue_scripts() {
+
+		$page = isset( $_REQUEST['page'] ) ? sanitize_text_field( $_REQUEST['page'] ) : '';
+
+		$current_screen = get_current_screen();
+
+
+		wp_enqueue_script( 'pf-admin', plugins_url( '/assets/js/pf-admin.js', __FILE__ ), 'jquery', time(), true );
+	}
+
+
+} //end main class
+
+new WP_Portfolio_Admin();
+
---
+++ wp-portfolio.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @wordpress-plugin
+ * Plugin Name:       WP Portfolio
+ * Plugin URI:        https://github.com/aminulsiam/cbxwpwritelog
+ * Description:       Create custom post, Show custom post, Show single post.
+ * Version:           1.0.0
+ * Author:            Aminul Haq Mallik
+ * Author URI:        https://github.com/aminulsiam
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       pf
+ * Domain Path:       /languages
+ */
+
+require_once 'class-plugin-admin.php';
+
+
