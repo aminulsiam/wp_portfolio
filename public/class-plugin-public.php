c74bf9f530226b36ea499fb3fc9b7d4771c9e6dd
aminulsiam <aminulhossain90@gmail.com>
2023-07-20 23:13:05 +0600

    Done, up to modal

--- public/class-plugin-public.php
+++ public/class-plugin-public.php
@@ -7,7 +7,7 @@ class WP_Portofolio_Public {
 	 * Write all admin hooks
 	 */
 	public function __construct() {
-		add_action( 'wp_enqueue_scripts', [ $this, 'pf_enqueue_style' ] );
+		add_action( 'wp_enqueue_scripts', [ $this, 'pf_enqueue_styles' ] );
 		add_action( 'wp_enqueue_scripts', [ $this, 'pf_enqueue_scripts' ] );
 
 		define( 'PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
@@ -19,15 +19,15 @@ class WP_Portofolio_Public {
 	/**
 	 * Register all styles
 	 */
-	public function pf_enqueue_style() {
-		wp_enqueue_style( 'bootstrap', PLUGIN_DIR_URL . '../assets/css/bootstrap.min.css', array(), time(), 'all' );
+	public function pf_enqueue_styles() {
+		wp_enqueue_style( 'pf-admin-css', plugins_url( '/assets/css/bootstrap.min.css', __FILE__ ), array(), time(), 'all' );
 	}
 
 	/**
-	 * Register all styles
+	 * Register all scripts
 	 */
 	public function pf_enqueue_scripts() {
-		wp_enqueue_script( 'bootstrap-js', PLUGIN_DIR_URL . '../assets/js/bootstrap.min.js', array(), time(), 'all' );
+		wp_enqueue_script( 'pf-admin', plugins_url( '/assets/css/bootstrap.min.js', __FILE__ ), 'jquery', time(), true );
 	}
 
 
